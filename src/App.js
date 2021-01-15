3415e1a77aacded066a5497af10baac15d344bf3
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-01-14 22:17:01 -0600

    Add basic styling to landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import friends1 from './friends1.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage:`linear-gradient(to left, rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.6)), url(${friends1})`}}>
       <h1><span className="App-one">one</span>layover</h1>
       <h2>coming soon!<i className="fas fa-plane"></i></h2>
     </div>
