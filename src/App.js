81007b039048b502ba1b1ad9553418f7f91dbd05
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-01-14 21:12:18 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><span className="App-one">one</span>layover</h1>
+      <h2>coming soon!<i className="fas fa-plane"></i></h2>
     </div>
   );
 }
