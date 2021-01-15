17cb86aacd9e4aff099afe0dad0e156f782b3d4b
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-01-15 10:21:52 -0600

    Update styling to landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import friends1 from './friends1.jpg';
+
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage:`linear-gradient(to left, rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.6)), url(${friends1})`}}>
+    <div className="App">
       <h1><span className="App-one">one</span>layover</h1>
-      <h2>coming soon!<i className="fas fa-plane"></i></h2>
+      <h2>landing soon!<i className="fas fa-plane"></i></h2>
     </div>
   );
 }
