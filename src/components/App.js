8cadbec2818a4c632d2b756261d81ae760166d25
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-02-01 23:43:05 -0600

    Use react-router-dom to set up client-side routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import '../styles/App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import { Route } from 'react-router-dom';
+// import '../styles/App.css';
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1><span className="App-logo-one">one</span><span className="App-logo-layover">layover</span></h1>
-      <h2>landing soon!<i className="fas fa-plane"></i></h2>
+      <Navbar />
+      {/* <h1><span className="App-logo-one">one</span><span className="App-logo-layover">layover</span></h1>
+      <h2>landing soon!<i className="fas fa-plane"></i></h2> */}
+      <Route exact path="/">
+        <h1>Landing Page</h1>
+      </Route>
+      <Route exact path="/login">
+        <h1>Login Page</h1>
+      </Route>
+      <Route exact path="/signup">
+        <h1>Sign up page</h1>
+      </Route>
+      <Route exact path="/layovers/">
+        <h1>List of layovers</h1>
+      </Route>
+      <Route exact path="/layovers/:layover_code">
+        <h1>Layover detail page</h1>
+      </Route>
     </div>
   );
 }
