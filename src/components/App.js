cab83702c9f373abc303911cd30e79f32e2e2659
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-02-01 22:25:50 -0600

    Create components and styles directory in src

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import '../styles/App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <h1><span className="App-one">one</span>layover</h1>
+      <h1><span className="App-logo-one">one</span><span className="App-logo-layover">layover</span></h1>
       <h2>landing soon!<i className="fas fa-plane"></i></h2>
     </div>
   );
