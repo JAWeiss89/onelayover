4532b4cf3f7063d8cae8d740d168b4640b4d3f5d
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-02-06 00:47:15 -0600

    Create landing page

---
+++ src/page-components/LandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/LandingPage.css';
+
+const LandingPage = () => {
+    return (
+        <div className="LandingPage">
+            <div className="LandingPage-header">
+                <div className="Landingpage-header-contents">
+                    <h1><span id="logo-one">one</span><span id="logo-layover">layover</span></h1>
+                    <p>The travel app for flight attendants by flight attendants</p>
+                    <div className="Landingpage-header-buttons">
+                        <Link to="/signup"><button id="signup-btn">SIGN UP</button> </Link>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
