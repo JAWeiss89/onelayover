fb813d895ac841ad729d4b32a2ddb22d94eaacd9
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-02-19 19:25:18 -0600

    Add search capability to filter layovers page

--- src/page-components/LayoversPage.js
+++ src/page-components/LayoversPage.js
@@ -5,27 +5,40 @@ import '../styles/LayoversPage.css';
 
 const LayoversPage = () => {
     const [layovers, setLayovers] = useState(null);
+    const [filteredLayovers, setFilteredLayovers] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         async function getLayovers() {
             let layovers = await OnelayoverAPI.getLayovers();
-            layovers.reverse(); 
+            console.log({layovers}); 
             setLayovers(layovers);
+            setFilteredLayovers(layovers);
         }
         getLayovers();
     }, [])
 
+    const handleChange = (e) => {
+
+        setFilteredLayovers(layovers.filter((layover) => {
+            return layover.layover_code.includes(e.target.value) 
+            || layover.city_name.toLowerCase().includes(e.target.value.toLowerCase())
+            || layover.country_name.toLowerCase().includes(e.target.value.toLowerCase());
+        }))
+        setSearchTerm(e.target.value);
+        
+    }
 
     return (
         <div className="LayoversPage">
             <h1>Layovers</h1>
             <div className="LayoversPage-search">
-                <input type="text" placeholder="e.g. CDG"/>
+                <input type="text" placeholder="e.g. CDG" onChange={handleChange} value={searchTerm}/>
                 <button><i className="fas fa-search"></i></button>
             </div>
-            {layovers ?
+            {filteredLayovers ?
             <div className="LayoversPage-layovers">
-                {layovers.map((layover) => {
+                {filteredLayovers.map((layover) => {
                     return (
                         < LayoverCard key={layover.layover_code} layover={layover}/>
                     )
